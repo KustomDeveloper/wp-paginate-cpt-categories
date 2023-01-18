82b832f6bdae3f58acab23fd4e94d9b6ec395d48
Michael Hicks <kustomdesigner@gmail.com>
2023-01-17 19:11:52 -0600

    Updating repo

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
 <?php 
 /*
-*  Plugin Name: Vue Boilerplate
+*  Plugin Name: WP Paginate CPT Categories
 *  Author: Michael Hicks
-*  Description: Vue Boilerplate
+*  Description: Paginates Custom Post Type Categories
 *  Version: 0.1
+*  Author: Michael Hicks
+*  Author URI: https://michaelhicks.me
+*  License: GPL v2 or later
+*  License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*  Text Domain: CPTCAT
 */
+
+//prefix - wpcpt
+
+if( ! defined( 'ABSPATH' ) ) {
+    return;
+}
 
 /**
  * Bootstrap Plugin
@@ -12,36 +23,152 @@
 require_once 'vendor/autoload.php';
 
 /*
-*  Add Env Variables 
-*/
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
-/*
 *  Load Scripts 
 */
-function load_plugin_scripts() {
+add_action( 'wp_enqueue_scripts', 'wpcpt_load_plugin_scripts' );
+
+function wpcpt_load_plugin_scripts() {
     wp_enqueue_script('vue', plugin_dir_url(__FILE__) . 'js/vue.global.prod.js');
     wp_enqueue_style('bootstrap-for-vue', 'https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css');
 }
-add_action( 'wp_enqueue_scripts', 'load_plugin_scripts' );
+
+/*
+*  Register CPT
+*/
+add_action( 'init', 'wpcpt_register_cpt' );
+
+function wpcpt_register_cpt() {
+    $args = array(
+
+        'label'                => __('Books', 'CPTCAT'),
+        'description'           => __( 'Default CPT', 'CPTCAT' ),
+        'public'                => true,
+        'hierarchical'          => false,
+        'exclude_from_search'   => true,
+        'publicly_queryable'    => false,
+        'show_ui'               => true,
+        'show_in_menu'          => true,
+        'show_in_nav_menus'     => false,
+        'show_in_admin_bar'     => false,
+        'show_in_rest'          => true,
+        'menu_position'         => null,
+        'menu_icon'             => 'default',
+        'capability_type'       => 'post',
+        'capabilities'          => array(),
+        'supports'              => array( 'title', 'editor', 'revisions' ),
+        'taxonomies'            => array(),
+        'has_archive'           => true,
+        'rewrite'               => array( 'slug' => 'books' ),
+        'query_var'             => true,
+        'can_export'            => true,
+        'delete_with_user'      => false,
+        'supports' => array( 'title', 'editor', 'custom-fields' ),
+        'template'              => array(),
+        'template_lock'         => false,
+
+    );
+
+    register_post_type( 'books', $args );
+}
+
+/**
+* Taxonomy: Books Categories
+*/
+function wpcpt_register_books_category() {
+
+    $labels = [
+        "name" => __( "Books Categories", "CPTCAT" ),
+        "singular_name" => __( "Books Category", "CPTCAT" ),
+    ];
+
+    $args = [
+        "label" => __( "Books Categories", "CPTCAT" ),
+        "labels" => $labels,
+        "public" => true,
+        "publicly_queryable" => true,
+        "hierarchical" => true,
+        "show_ui" => true,
+        "show_in_menu" => true,
+        "show_in_nav_menus" => true,
+        "query_var" => true,
+        "rewrite" => [ 'slug' => 'books_category', 'with_front' => true, ],
+        "show_admin_column" => false,
+        "show_in_rest" => true,
+        "rest_base" => "books_category",
+        "rest_controller_class" => "WP_REST_Terms_Controller",
+        "show_in_quick_edit" => false,
+
+            ];
+    register_taxonomy( "books_category", [ "books" ], $args );
+}
+add_action( 'init', 'wpcpt_register_books_category' );
+
+/*
+*  Add custom meta box
+*/
+function wpcpt_add_metaboxes_to_cpt() {
+    $post_type = 'books';
+    add_meta_box(
+        'yt_url',               
+        'YouTube URL',     
+        'wpcpt_youtube_url_callback',  
+        $post_type,
+        'side',
+        'high'
+    );
+}
+add_action( 'add_meta_boxes', 'wpcpt_add_metaboxes_to_cpt' );
+
+/*
+*   Metabox Callbacks
+*/
+function wpcpt_youtube_url_callback( $post ) {
+    $value = get_post_meta( $post->ID, 'yt_url', true ); ?>
+    <label for="yt_url">Add YouTube URL Here</label><br/>
+    <input name="yt_url" id="yt_url" value="<?php echo $value; ?>" />
+    <?php
+}
+
+/*
+*   Save Metaboxes
+*/
+function wpcpt_youtube_url_savedata( $post_id ) {
+    if ( array_key_exists( 'yt_url', $_POST ) ) {
+        update_post_meta(
+            $post_id,
+            'yt_url',
+            $_POST['yt_url']
+        );
+    }
+}
+add_action( 'save_post', 'wpcpt_youtube_url_savedata' );
+
+/*
+*  Register Custom Meta Box so it shows in wp rest api
+*/
+add_action('init', 'wpcpt_register_metaboxes_for_rest_api');
+
+function wpcpt_register_metaboxes_for_rest_api() {
+    add_action( 'rest_api_init', function () {
+        register_rest_field('books', 'yt_url', array(
+            'get_callback' => function( $post_arr ) {
+                return get_post_meta( $post_arr['id'], 'yt_url', true );
+            },
+        ) );
+    } );
+}
 
 /*
 *  Add Shortcode
 */
-add_shortcode('weatherApi' , 'weather_api');
-function weather_api() {
+add_shortcode('wpcptCat', 'wpcpt_show_cpt');
 
-    //get api key
-    $api_key = $_ENV['WEATHER_API'];
-
-    //get blog title
-    $blog_title = get_bloginfo( 'name' );
-
+function wpcpt_show_cpt() {
     echo(
-        "<div id='vue-app'><weather-api apikey='$api_key' /></div>"
+        "<div id='vue-app'><wpcpt-cat /></div>"
     );
 
     wp_enqueue_script('app', plugin_dir_url(__FILE__) . '/js/app.js');
 }
+
 
--- js/app.js
+++ js/app.js
@@ -1,18 +1,15 @@
 /*
-*  Vue Boilerplate 
+*  Wp Cpt Categories
 */
 
 
 //Components
-const WeatherApi = {
-    props: ['apikey'],
+const WpcptCat = {
     data() {
         return {
-          information: undefined,
           posts: {
             postArray: undefined
           },
-          completedWeatherCall: false,
           completedPostsCall: false,
         }
       },
@@ -25,28 +22,9 @@ const WeatherApi = {
         // }
       },
       methods: {
-        fetchWeather() {
-          fetch(`http://api.openweathermap.org/geo/1.0/direct?q=London&limit=1&appid=${this.apikey}`)
-            .then(res => res.json())
-            .then(res => {
-
-              fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${res[0].lat}&lon=${res[0].lon}&appid=${this.apikey}`)
-                .then(response => response.json())
-                .then(response => {
-
-                    this.information =  Math.ceil((response.main.temp - 273.15) * 9/5 + 32);
-                    this.completedWeatherCall = true;
-
-                }).catch(function(error) {
-                    console.error('Weather Report Error:', error);
-                })
-            }).catch(function(error) {
-              console.error('LatLong Error:', error);
-            })
-        },
 
         fetchPosts() {
-            fetch(`http://localhost/plugin-development/wp-json/wp/v2/posts`)
+            fetch(`http://localhost/plugin-development/wp-json/wp/v2/books`)
                 .then(res => res.json())
                 .then(res => {
                     this.posts = res
@@ -56,13 +34,7 @@ const WeatherApi = {
       },
 
       template: 
-        `<div id='weather-plugin' class='container'>
-
-            <div v-if="completedWeatherCall">
-              <div class='alert alert-primary' role='alert'>
-                  Currently the temp in London is: {{information}}
-              </div>
-            </div>
+        `<div id='Wpcpt-Categories' class='container'>
     
             <div v-if="completedPostsCall">
               <ul>
@@ -72,14 +44,13 @@ const WeatherApi = {
         </div>`
      ,
       mounted() {
-        this.fetchWeather();
         this.fetchPosts();
       }
 }
 
 RootComponent = {
     components: {
-        WeatherApi,
+      WpcptCat,
     },
   
 }
